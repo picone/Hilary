51f345a823cb2b1913360109752913ed8de39858
Chien <467360687@qq.com>
2015-08-17 22:53:22 +0800

    完成消星星模块=_~

--- app/src/main/java/com/chien/hilary/StarSurfaceView.java
+++ app/src/main/java/com/chien/hilary/StarSurfaceView.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class StarSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
@@ -31,8 +33,8 @@ public class StarSurfaceView extends SurfaceView implements SurfaceHolder.Callba
                 Color.GREEN,
                 Color.RED,
                 Color.YELLOW,
-                Color.WHITE,
-                Color.BLUE
+                Color.BLUE,
+                Color.DKGRAY
         };
         Random rnd=new Random();
         for(int i=0;i<ROW;i++){
@@ -44,11 +46,45 @@ public class StarSurfaceView extends SurfaceView implements SurfaceHolder.Callba
 
     @Override
     public boolean onTouchEvent(MotionEvent event){
-        //https://github.com/mlxy/Poping/blob/master/Poping/src/com/mlxy/poping/GameSurfaceView.java
         int x=(int)(event.getX()/star_width);
         int y=(int)(event.getY()/star_height);
-        if(stars[y][x]>=0){
-
+        if(event.getAction()==MotionEvent.ACTION_DOWN&&x<COL&&y<ROW&&stars[y][x]>=0){
+            ArrayList<Point> same=new ArrayList<>();
+            same.add(new Point(x,y));
+            for(int i=0;i<same.size();i++){
+                judge(same.get(i),same);
+            }
+            if(same.size()>1){
+                int i=same.size();
+                while(i-->0){
+                    stars[same.get(i).y][same.get(i).x]=-1;
+                }
+                for(i=0;i<COL;i++){
+                    for(int j=1;j<ROW;j++){
+                        if(stars[j][i]==-1){
+                            for(int k=j;k>0;k--){
+                                stars[k][i]=stars[k-1][i];
+                            }
+                            stars[0][i]=-1;
+                        }
+                    }
+                }
+                int move_num=0;
+                for(i=0;i<COL-move_num;){
+                    if(stars[ROW-1][i]==-1){//如果是空列的话
+                        for(int j=i+1;j<COL;j++){//后面的列前移
+                            for(int k=ROW-1;k>=0&&stars[k][j]>=0;k--){
+                                stars[k][j-1]=stars[k][j];
+                                stars[k][j]=-1;
+                            }
+                        }
+                        move_num++;
+                    }else{
+                        i++;
+                    }
+                }
+                redraw();
+            }
             return true;
         }else{
             return super.onTouchEvent(event);
@@ -63,12 +99,41 @@ public class StarSurfaceView extends SurfaceView implements SurfaceHolder.Callba
                 for(int j=0;j<COL;j++){
                     if(stars[i][j]>=0){
                         paint.setColor(colors[stars[i][j]]);
-                        canvas.drawRect(j*star_width,i*star_height,(j+1)*star_width,(i+1)*star_height,paint);
+                    }else{
+                        paint.setColor(Color.BLACK);
                     }
+                    canvas.drawRect(j*star_width,i*star_height,(j+1)*star_width,(i+1)*star_height,paint);
                 }
             }
             getHolder().unlockCanvasAndPost(canvas);
         }
+    }
+
+    private void judge(Point p,ArrayList<Point> same){
+        if(p.y>0&&stars[p.y][p.x]==stars[p.y-1][p.x]){
+            addList(new Point(p.x,p.y-1),same);
+        }
+        if(p.x>0&&stars[p.y][p.x]==stars[p.y][p.x-1]){
+            addList(new Point(p.x-1,p.y),same);
+        }
+        if(p.y+1<ROW&&stars[p.y][p.x]==stars[p.y+1][p.x]){
+            addList(new Point(p.x,p.y+1),same);
+        }
+        if(p.x+1<COL&&stars[p.y][p.x]==stars[p.y][p.x+1]){
+            addList(new Point(p.x+1,p.y),same);
+        }
+    }
+
+    private void addList(Point data,ArrayList<Point> list){
+        int i=list.size();
+        boolean exists=false;
+        while(i-->0){
+            if(list.get(i).equals(data)){
+                exists=true;
+                break;
+            }
+        }
+        if(!exists)list.add(data);
     }
 
     @Override
