ae2a236566a91be4ad649f065a56f72d8b065e8b
Chien <467360687@qq.com>
2015-08-18 16:26:45 +0800

    总体完成~

--- app/src/main/java/com/chien/hilary/EndFragment.java
+++ app/src/main/java/com/chien/hilary/EndFragment.java
@@ -1,15 +1,20 @@
 package com.chien.hilary;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * 最后一个Fragment
  */
 public class EndFragment extends Fragment{
+
+    private Button call;
 
     public EndFragment(){
         // Required empty public constructor
@@ -24,6 +29,18 @@ public class EndFragment extends Fragment{
                 ((MainActivity)getActivity()).switchFragment(5);
             }
         });
+        call=(Button)view.findViewById(R.id.call);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view,Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        call.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(Intent.ACTION_CALL).setData(Uri.parse("tel:620820")));
+            }
+        });
     }
 }
